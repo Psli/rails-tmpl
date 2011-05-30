a1d31db06de8f990448d7f4324438b0430a65960
Psi <xiepengxi@gmail.com>
2011-05-31 00:41:49 +0800

    first commit

---
+++ m31.rb
@@ -0,0 +1,42 @@
+# remove files
+run "rm README"
+run "rm public/index.html"
+run "rm app/assets/images/rails.png"
+run "cp config/database.yml config/database.yml.example"
+
+# install gems
+run "rm Gemfile"
+run "curl -L https://github.com/Psli/rails-tmpl/raw/master/Gemfile31 > Gemfile"
+
+# bundle install
+run "bundle install"
+
+gsub_file 'config/application.rb', /(config.generators.test_framework = false)/,
+                                   "config.generators.test_framework = :rspec"
+
+# generate rspec
+generate "rspec:install"
+
+# add time format
+# environment 'Time::DATE_FORMATS.merge!(:default => "%Y-%m-%d %I:%M %p", :ymd => "%Y-%m-%d")'
+
+# .gitignore
+append_file '.gitignore', <<-CODE
+config/database.yml
+coverage/*
+*.tmproj
+cmd.txt
+Thumbs.db
+.DS_Store
+CODE
+
+# keep tmp and log
+run "touch tmp/.gitkeep"
+run "touch log/.gitkeep"
+
+# git commit
+git :init
+git :add => '.'
+git :add => 'tmp/.gitkeep -f'
+git :add => 'log/.gitkeep -f'
+git :commit => "-a -m 'init commit'"
