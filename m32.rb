fd11a1e92e6ef4fc582820a3f78656eece4686eb
Psi <psi.xie@gmail.com>
2011-12-29 18:14:43 +0800

    add raisl 3.2

---
+++ m32.rb
@@ -0,0 +1,37 @@
+# remove files
+run "rm README"
+run "rm public/index.html"
+run "rm app/assets/images/rails.png"
+run "cp config/database.yml config/database.yml.example"
+
+# install gems
+run "rm Gemfile"
+run "curl -L https://raw.github.com/Psli/rails-tmpl/master/Gemfile32 > Gemfile"
+
+# bundle install
+run "bundle install"
+
+# generate rspec
+generate "rspec:install"
+
+# add time format
+# environment 'Time::DATE_FORMATS.merge!(:default => "%Y-%m-%d %I:%M %p", :ymd => "%Y-%m-%d")'
+
+# .gitignore
+append_file '.gitignore', <<-CODE
+config/database.yml
+coverage/*
+*.tmproj
+cmd.txt
+Thumbs.db
+.DS_Store
+CODE
+
+# keep tmp and log
+# run "touch tmp/.gitkeep"
+# run "touch log/.gitkeep"
+
+# git commit
+git :init
+git :add => '.'
+git :commit => "-a -m 'init commit'"
